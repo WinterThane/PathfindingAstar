f62d8c0c3fad19a5cbe5bb7667a5e9968d95cfef
Winter Thane <saso.basic.2012@gmail.com>
2022-03-07 18:16:59 +0100

    Scene editor in the making

---
+++ PathfindingAstar/Editor/SceneEditor.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+
+namespace PathfindingAstar.Editor
+{
+    enum EditMode
+    {
+        Select,
+        Move,
+    }
+
+    public class SceneEditor
+    {
+        EditMode editMode;
+
+        public SceneEditor()
+        {
+            //KeyboardInput.AddKey(Keys.Q);
+            //KeyboardInput.AddKey(Keys.W);
+
+            Keys[] allKeys = (Keys[])Enum.GetValues(typeof(Keys)); // listen to all possible keys
+            foreach (var key in allKeys)
+            {
+                KeyboardInput.AddKey(key);
+            }
+
+            KeyboardInput.KeyPress += KeyboardInput_KeyPress;
+        }
+
+        private void KeyboardInput_KeyPress(Keys key, KeyboardState keyState)
+        {
+            if (key == Keys.Q)
+            {
+                editMode = EditMode.Select;
+            }
+            else if (key == Keys.W)
+            {
+                editMode = EditMode.Move;
+            }
+        }
+
+        public void Draw(SpriteBatch spriteBatch)
+        {
+            string text = string.Format("Mode: {0}", editMode);
+            spriteBatch.DrawString(Style.FontLarge, text, new Vector2(10, 10), Color.White);
+        }
+    }
+}
--- PathfindingAstar/SceneNavigation.cs
+++ PathfindingAstar/SceneNavigation.cs
@@ -1,18 +1,24 @@
 ﻿using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content;
+using PathfindingAstar.Editor;
 
 namespace PathfindingAstar
 {
     public class SceneNavigation : Scene
     {
+        SceneEditor sceneEditor;
+
         public override void BuildScene(GraphicsDevice graphicsDevice, ContentManager contant)
         {
-
+            sceneEditor = new SceneEditor();
         }
 
         public override void Update(GameTime gameTime)
         {
+            KeyboardInput.Update();
+            MouseInput.Update();
+
             foreach (Actor actor in Actor.Actors)
             {
                 actor.Update();
@@ -29,6 +35,8 @@ namespace PathfindingAstar
             {
                 actor.Draw(spriteBatch);
             }
+
+            sceneEditor.Draw(spriteBatch);
 
             spriteBatch.End();
         }
